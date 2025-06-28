e0de782594439dd5af30af5f8498abbe157d7b02
shekharpokharkar <132981368+shekharpokharkar@users.noreply.github.com>
2025-06-28 16:21:55 +0530

    Update One2OneMappingApplication.java
    Try to learn git through IDE

--- One2OneMapping/src/main/java/com/example/demo/One2OneMappingApplication.java
+++ One2OneMapping/src/main/java/com/example/demo/One2OneMappingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class One2OneMappingApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Inside developer shekhar Branch");
+		System.out.println("++++++++++++++Inside developer shekhar Branch++++++++++++++++");
 		SpringApplication.run(One2OneMappingApplication.class, args);
 	}
 
